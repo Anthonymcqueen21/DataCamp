7a88e99550e94a4db69201840dedbd25c82101b1
Blake Cannon <wblakecannon@icloud.com>
2017-11-25 16:05:30 -0700

    completed importing-data-in-python-(part-2)/3-diving-deep-into-the-twitter-api

---
+++ 8-importing-data-in-python-(part-2)/3-diving-deep-into-the-twitter-api/a-little-bit-of-twitter-text-analysis.py
@@ -0,0 +1,21 @@
+import re
+
+def word_in_text(word, tweet):
+    word = word.lower()
+    text = tweet.lower()
+    match = re.search(word, tweet)
+
+    if match:
+        return True
+    return False
+
+# Initialize list to store tweet counts
+[clinton, trump, sanders, cruz] = [0, 0, 0, 0]
+
+# Iterate through df, counting the number of tweets in which
+# each candidate is mentioned
+for index, row in df.iterrows():
+    clinton += word_in_text('clinton', row['text'])
+    trump += word_in_text('trump', row['text'])
+    sanders += word_in_text('sanders', row['text'])
+    cruz += word_in_text('cruz', row['text'])
---
+++ 8-importing-data-in-python-(part-2)/3-diving-deep-into-the-twitter-api/streaming-tweets.py
@@ -0,0 +1,39 @@
+# Import package
+import tweepy
+import json
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api=None):
+        super(MyStreamListener, self).__init__()
+        self.num_tweets = 0
+        self.file = open("tweets.txt", "w")
+    def on_status(self, status):
+        tweet = status._json
+        self.file.write(json.dumps(tweet) + '\n')
+        tweet_list.append(status)
+        self.num_tweets += 1
+        if self.num_tweets < 100:
+            return True
+        else:
+            return False
+        self.file.close()
+
+# Store OAuth authentication credentials in relevant variables (these are DataCamp tokens)
+access_token = "1092294848-aHN7DcRP9B4VMTQIhwqOYiB14YkW92fFO8k8EPy"
+access_token_secret = "X4dHmhPfaksHcQ7SCbmZa2oYBBVSD2g8uIHXsp5CTaksx"
+consumer_key = "nZ6EA0FxZ293SxGNg8g8aP0HM"
+consumer_secret = "fJGEodwe3KiKUnsYJC3VRndj7jevVvXbK2D5EiJ2nehafRgA6i"
+
+# Pass OAuth details to tweepy's OAuth handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Initialize Stream listener
+l = MyStreamListener()
+
+# Create you Stream object with authentication
+stream = tweepy.Stream(auth, l)
+
+
+# Filter Twitter Streams to capture data by the keywords:
+stream.filter(track=['clinton', 'trump', 'sanders', 'cruz'])

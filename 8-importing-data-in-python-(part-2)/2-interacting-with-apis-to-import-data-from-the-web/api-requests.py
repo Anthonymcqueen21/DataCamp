ecb6c4fe4859ae960410eee73af0b805e0319afc
Blake Cannon <wblakecannon@icloud.com>
2017-11-25 15:34:58 -0700

    completed importing-data-in-python-(part-2)/2-interacting-with-apis-to-import-data-from-the-web

---
+++ 8-importing-data-in-python-(part-2)/2-interacting-with-apis-to-import-data-from-the-web/api-requests.py
@@ -0,0 +1,11 @@
+# Import requests package
+import requests
+
+# Assign URL to variable: url
+url = 'http://www.omdbapi.com/?apikey=ff21610b&t=social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Print the text of the response
+print(r.text)

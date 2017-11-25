1a7106b50ce16cce19f20b908a68b7e9ecc0e536
Blake Cannon <wblakecannon@icloud.com>
2017-11-25 13:34:49 -0700

    completed statistical-thinking-in-python-(part-2)/3-introduction-to-hypothesis-testing

---
+++ 7-statistical-thinking-in-python-(part-2)/3-introduction-to-hypothesis-testing/a-one-sample-bootstrap.py
@@ -0,0 +1,12 @@
+# Make an array of translated impact forces: translated_force_b
+translated_force_b = force_b - np.mean(force_b) + 0.55
+
+# Take bootstrap replicates of Frog B's translated impact forces: bs_replicates
+bs_replicates = draw_bs_reps(translated_force_b, np.mean, 10000)
+
+# Compute fraction of replicates that are less than the observed Frog B force: p
+p = np.sum(bs_replicates <= np.mean(force_b)) / 10000
+
+# Print the p-value
+print('p = ', p)
+

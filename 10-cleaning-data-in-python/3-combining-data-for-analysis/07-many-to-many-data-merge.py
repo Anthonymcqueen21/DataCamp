cbd3a49a9e1657c8502477c7ea71f34b27fe105a
Blake Cannon <blake.cannon@caprinomics.com>
2018-01-10 08:56:37 -0700

    updated solution

--- 10-cleaning-data-in-python/3-combining-data-for-analysis/07-many-to-many-data-merge.py
+++ 10-cleaning-data-in-python/3-combining-data-for-analysis/07-many-to-many-data-merge.py
@@ -15,3 +15,11 @@ INSTRUCTIONS
 -Merge the m2m and survey DataFrames on the 'ident' column of m2m and 'taken' column of survey.
 -Hit 'Submit Answer' to print the first 20 lines of the merged DataFrame!
 '''
+# Merge site and visited: m2m
+m2o = pd.merge(left=site, right=visited, left_on='name', right_on='site')
+
+# Merge m2m and survey: m2m
+m2m = pd.merge(left=m2o, right=survey, left_on='ident', right_on='taken')
+
+# Print the first 20 lines of m2m
+print(m2m.head(20))

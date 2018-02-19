8678dd8cc1a66087f8ed17bdba5d1881eea1f174
Blake Cannon <blake.cannon@caprinomics.com>
2018-02-19 16:16:37 -0700

    completed 20-deep-learning-in-python/03-building-deep-learning-models-with-keras

---
+++ 20-deep-learning-in-python/03-building-deep-learning-models-with-keras/02-compiling-the-model.py
@@ -0,0 +1,28 @@
+'''
+Compiling the model
+
+You're now going to compile the model you specified earlier. To compile the model, you need to specify the optimizer and loss function to use. In the video, Dan mentioned that the Adam optimizer is an excellent choice. You can read more about it as well as other keras optimizers here, and if you are really curious to learn more, you can read the original paper that introduced the Adam optimizer.
+
+In this exercise, you'll use the Adam optimizer and the mean squared error loss function. Go for it!
+
+INSTRUCTIONS
+100XP
+Compile the model using model.compile(). Your optimizer should be 'adam' and the loss should be 'mean_squared_error'.
+'''
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
+# Specify the model
+n_cols = predictors.shape[1]
+model = Sequential()
+model.add(Dense(50, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Verify that model contains information from compiling
+print("Loss function: " + model.loss)

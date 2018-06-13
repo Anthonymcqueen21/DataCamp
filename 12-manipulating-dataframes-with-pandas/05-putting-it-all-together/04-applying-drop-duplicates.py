08060896580b1be0ff39b2f65f052713cfb11853
VishalAmbavade <vaambavade@gmail.com>
2018-06-13 15:36:31 +0530

    Update 04-applying-drop-duplicates.py

--- 12-manipulating-dataframes-with-pandas/05-putting-it-all-together/04-applying-drop-duplicates.py
+++ 12-manipulating-dataframes-with-pandas/05-putting-it-all-together/04-applying-drop-duplicates.py
@@ -15,7 +15,7 @@ Print ev_gen_uniques. This has been done for you, so hit 'Submit Answer' to see 
 ev_gen = medals[['Event_gender', 'Gender']]
 
 # Drop duplicate pairs: ev_gen_uniques
-ev_gen_uniques = ev_gen.drop_duplicates(ev_gen)
+ev_gen_uniques = ev_gen.drop_duplicates()
 
 # Print ev_gen_uniques
 print(ev_gen_uniques)

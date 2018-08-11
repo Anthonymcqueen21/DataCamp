34fcd6d2bd382af329658f1a4081e4565c194bf4
Aryan Khandal <aryankhandal0@gmail.com>
2018-08-12 01:56:00 +0530

    Update the-power-of-sql-lies-in-relationships-between-tables-inner-join.py

--- 05-importing-data-in-python-(part-1)/3-working-with-relational-databases-in-python/the-power-of-sql-lies-in-relationships-between-tables-inner-join.py
+++ 05-importing-data-in-python-(part-1)/3-working-with-relational-databases-in-python/the-power-of-sql-lies-in-relationships-between-tables-inner-join.py
@@ -39,7 +39,7 @@ engine = create_engine('sqlite:///../_datasets/Chinook.sqlite')
 # Open engine in context manager
 # Perform query and save results to DataFrame: df
 with engine.connect() as con:
-    rs = con.execute("SELECT Title, Name FROM Album INNER JOIN Artist on Album.ArtistID = Artist.ArtistID", engine)
+    rs = con.execute("SELECT Title, Name FROM Album INNER JOIN Artist on Album.ArtistID = Artist.ArtistID")
     df = pd.DataFrame(rs.fetchall())
     df.columns = rs.keys()
 

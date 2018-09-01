8afe0affafa4f14a8b441643a484e101a7ff8d15
Aditya Kabra <32962476+adi0602@users.noreply.github.com>
2018-09-01 08:57:26 +0530

    Corrected a minor error in code

--- 11-pandas-foundations/04-case-study-sunlight-in-austin/09-weekly-average-temperature-and-visibility.py
+++ 11-pandas-foundations/04-case-study-sunlight-in-austin/09-weekly-average-temperature-and-visibility.py
@@ -20,7 +20,7 @@ INSTRUCTIONS
 import matplotlib.pyplot as plt
 
 # Select the visibility and dry_bulb_faren columns and resample them: weekly_mean
-weekly_mean = df_clean[['visibility','dry_bulb_faren']].resample('W')
+weekly_mean = df_clean[['visibility','dry_bulb_faren']].resample('W').mean()
 
 # Print the output of weekly_mean.corr()
 print(weekly_mean.corr())

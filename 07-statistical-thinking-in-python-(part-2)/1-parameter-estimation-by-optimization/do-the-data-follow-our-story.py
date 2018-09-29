52d932f49e36e82a9a486aa05eac644e2111d7da
Aryan Khandal <aryankhandal0@gmail.com>
2018-09-29 16:18:51 +0530

    Update do-the-data-follow-our-story.py
    https://campus.datacamp.com/courses/statistical-thinking-in-python-part-2/parameter-estimation-by-optimization?ex=3
    This is the Updated solution.

--- 07-statistical-thinking-in-python-(part-2)/1-parameter-estimation-by-optimization/do-the-data-follow-our-story.py
+++ 07-statistical-thinking-in-python-(part-2)/1-parameter-estimation-by-optimization/do-the-data-follow-our-story.py
@@ -22,26 +22,20 @@ in addition to x and y inside plt.plot().
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Plot the theoretical CDFs
+# Create an ECDF from real data: x, y
+x, y = ecdf(nohitter_times)
+
+# Create a CDF from theoretical samples: x_theor, y_theor
+x_theor, y_theor = ecdf(inter_nohitter_time)
+
+# Overlay the plots
 plt.plot(x_theor, y_theor)
 plt.plot(x, y, marker='.', linestyle='none')
+
+# Margins and axis labels
 plt.margins(0.02)
 plt.xlabel('Games between no-hitters')
 plt.ylabel('CDF')
-
-# Take samples with half tau: samples_half
-samples_half = np.random.exponential(tau/2, 10000)
-
-# Take samples with double tau: samples_double
-samples_double = np.random.exponential(tau*2, 10000)
-
-# Generate CDFs from these samples
-x_half, y_half = ecdf(samples_half)
-x_double, y_double = ecdf(samples_double)
-
-# Plot these CDFs as lines
-_ = plt.plot(x_half, y_half)
-_ = plt.plot(x_double, y_double)
 
 # Show the plot
 plt.show()

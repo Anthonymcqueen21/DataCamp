f8ea2d8afdd95198ad61354cf7d38ce5bef2ec32
jsraucci <42627271+jsraucci@users.noreply.github.com>
2018-10-12 14:50:00 -0300

    Update the-vote-for-the-civil-rights-act-in-1964.py

--- 07-statistical-thinking-in-python-(part-2)/4-hypothesis-test-examples/the-vote-for-the-civil-rights-act-in-1964.py
+++ 07-statistical-thinking-in-python-(part-2)/4-hypothesis-test-examples/the-vote-for-the-civil-rights-act-in-1964.py
@@ -1,17 +1,15 @@
-# Compute mean of all forces: mean_force
-mean_force = np.mean(forces_concat)
+# Construct arrays of data: dems, reps
+dems = np.array([True] * 153 + [False] * 91)
+reps = np.array([True] * 136 + [False] * 35)
 
-# Generate shifted arrays
-force_a_shifted = force_a - np.mean(force_a) + mean_force
-force_b_shifted = force_b - np.mean(force_b) + mean_force
+def frac_yea_dems(dems, reps):
+    """Compute fraction of Democrat yea votes."""
+    frac = np.sum(dems) / len(dems)
+    return frac
 
-# Compute 10,000 bootstrap replicates from shifted arrays
-bs_replicates_a = draw_bs_reps(force_a_shifted, np.mean, size=10000)
-bs_replicates_b = draw_bs_reps(force_b_shifted, np.mean, size=10000)
-
-# Get replicates of difference of means: bs_replicates
-bs_replicates = bs_replicates_a - bs_replicates_b
+# Acquire permutation samples: perm_replicates
+perm_replicates = draw_perm_reps(dems, reps, frac_yea_dems, size=10000)
 
 # Compute and print p-value: p
-p = np.sum(bs_replicates >= empirical_diff_means) / len(bs_replicates)
+p = np.sum(perm_replicates <= 153/244) / len(perm_replicates)
 print('p-value =', p)
